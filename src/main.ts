aff5f460a09168ead19429796d091659e5181c7e
Sage Wall <swall@esri.com>
2025-08-18 11:00:17 -0600

    feat: add map, layer list, legend

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "@arcgis/map-components/components/arcgis-layer-list";
+import "@arcgis/map-components/components/arcgis-legend";
+import "@arcgis/map-components/components/arcgis-map";
+import "@esri/calcite-components/components/calcite-shell";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const shell = document.createElement("calcite-shell");
+
+const map = document.createElement("arcgis-map");
+map.itemId = "e66cdb0c412245c6b0d8e58346a115fb";
+
+const legend = document.createElement("arcgis-legend");
+legend.referenceElement = map;
+legend.slot = "top-right";
+map.appendChild(legend);
+
+const layerList = document.createElement("arcgis-layer-list");
+layerList.referenceElement = map;
+layerList.slot = "top-left";
+map.appendChild(layerList);
+
+shell.appendChild(map);
+app.appendChild(shell);
